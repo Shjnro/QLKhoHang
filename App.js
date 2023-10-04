cda4791d8d72c6040298198c5c01f23500fe9bfb
NguyenHoangKinh29808 <21211tt4678@mail.tdc.edu.vn>
2023-10-04 11:16:42 +0700

    mang hinh danh sach don hang chua hoan thanh

--- App.js
+++ App.js
@@ -5,14 +5,7 @@ import OderLiat from './Screens/OrderLiat'
 export default function App() {
   return (
     <View style={styles.container}>
-    {/* view header */}
-    <View style={styles.header}/>
-    {/*view main */}
-    <View style={styles.main}>
-      <OderLiat/>
-    </View>
-    {/* view footer */}
-    <View style={styles.footer}/>
+    <OderLiat/>
     </View>
   );
 }
@@ -20,23 +13,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  header: {
-  backgroundColor:'black',
-  height:'10%',
-  width:'100%',
-  },
-  main: {
-  height:'80%',
-  width:'100%',
-  display: 'flex',
-  flexDirection: 'row',
-  backgroundColor:'orange',
-
-  },
-  footer: {
-  height:'10%',
-  width:'100%',
-  backgroundColor:'blue',
   },
 });
--- Screens/OrderLiat.js
+++ Screens/OrderLiat.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import { Button } from "@rneui/base";
+import React from "react";
 import {
   SafeAreaView,
   View,
@@ -6,124 +7,177 @@ import {
   StyleSheet,
   Text,
   StatusBar,
-} from 'react-native';
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
 
 const DATA = [
   {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
   },
   {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
   },
 ];
 
-type ItemProps = {title: string};
+type ItemProps = { title: string };
 
-const Item = ({title}: ItemProps) => (
+const Item = ({ title }: ItemProps) => (
   <View style={styles.item}>
     <Text style={styles.title}>{title}</Text>
   </View>
 );
 
 const OderLiat = () => {
-  const Boxes = () =>{
-    return(
-      <View style={[styles.boxes,{backgroundColor:'yellow'}]}>
-      <View style={[styles.boxesitem,{backgroundColor:'blue'}]}></View>
-      <View style={[styles.boxesitem,{backgroundColor:'red'}]}></View>
+  const Boxes = () => {
+    return (
+      <View style={styles.boxes}>
+        <Pressable
+          style={styles.button}
+          onPress={() => Alert.alert("Left button pressed")}
+        >
+          <Text style={styles.text}>hello</Text>
+        </Pressable>
+        <Pressable
+          style={styles.button}
+          onPress={() => Alert.alert("Left button pressed")}
+        >
+          <Text style={styles.text}>hello</Text>
+        </Pressable>
       </View>
     );
   };
-  const ListItem = () =>{
-    return(
+  const ListItem = () => {
+    return (
       <SafeAreaView style={styles.boxesList}>
-      <FlatList
-        data={DATA}
-        renderItem={({item}) => <Item title={item.title} />}
-        keyExtractor={item => item.id}
-      />
+        <FlatList
+          data={DATA}
+          renderItem={({ item }) => <Item  title={item.title} ></Item>}
+          keyExtractor={(item) => item.id}
+        />
       </SafeAreaView>
     );
   };
   return (
-    <SafeAreaView  style={styles.container}>
-    <Boxes/>
-    <ListItem/>
+    <SafeAreaView style={styles.container}>
+      <Boxes />
+      <ListItem />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container:{
-  width:'100%',
+  container: {
+    margin: 10,
   },
   item: {
-    height:60,
-    marginTop:10,
-    backgroundColor:'yellow'
+    height: 120,
+    margin: 10,
+    borderRadius: 10,
+    backgroundColor: "red",
   },
   title: {
-    fontSize: 32,
+    fontSize: 23,
   },
   boxes: {
-    height:'12%',
-    flexDirection:'row',
+    height: "9%",
+    flexDirection: "row",
+    // backgroundColor: "blue",
   },
-  ListItem:{
-  height:'85%'
 
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 79.4,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "white",
   },
-  boxesitem:{
-    width:'50%',
-  },
-  boxesTop:{
-    height:'15%',
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "balck",
   },
 });
 
